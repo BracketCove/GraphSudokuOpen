47ac5c52e6e742d4388e7d8f31f73e0ccf18609b
Sory <sorinnoroc1@gmail.com>
2021-10-22 22:32:03 +0300

    Created the node class for the graph

--- app/src/main/java/com/bracketcove/graphsudoku/domain/SudokuNode.kt
+++ app/src/main/java/com/bracketcove/graphsudoku/domain/SudokuNode.kt
@@ -11,20 +11,17 @@ import java.io.Serializable
  *
  */
 data class SudokuNode(
-        val x: Int,
-        val y: Int,
-        var color: Int = 0,
-        var readOnly: Boolean = true
+    val x: Int,
+    val y: Int,
+    var color: Int = 0,
+    var readOnly: Boolean = true
 ): Serializable {
-    /**
-     * The x value is *100 to allow for uniqueness
-     */
     override fun hashCode(): Int {
         return getHash(x, y)
     }
 }
 
-internal fun getHash(x: Int, y: Int):Int {
-    val newX = x*100
+internal fun getHash(x: Int, y: Int): Int {
+    val newX = x * 100
     return "$newX$y".toInt()
 }
