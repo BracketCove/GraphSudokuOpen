c11be2cba1deec69c480e5b086a77bf6cb4e2bf7
Sory <sorinnoroc1@gmail.com>
2021-10-24 16:02:41 +0300

    Created the basic logic, following the OCP

--- app/src/main/java/com/bracketcove/graphsudoku/common/BaseLogic.kt
+++ app/src/main/java/com/bracketcove/graphsudoku/common/BaseLogic.kt
@@ -2,7 +2,7 @@ package com.bracketcove.graphsudoku.common
 
 import kotlinx.coroutines.Job
 
-abstract class BaseLogic<EVENT>  {
+abstract class BaseLogic<EVENT> {
     protected lateinit var jobTracker: Job
     abstract fun onEvent(event: EVENT)
 }
